ad81d77456e83705b410e93e07f508de7b59af40
Ganburel Darhansukh <g.darhansukh@gmail.com>
2024-11-04 15:35:44 +0100

    utilisation de express

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const sampleEmployee = {
 function App() {
 	const [employee, setEmployee] = useState(sampleEmployee);
 	const getEmployee = () => {
-		fetch("https://randomuser.me/api?nat=en")
+		fetch("http://localhost:3310/api/employees")
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data);
